3d8feed1ec5fc198aefce45441a88ccb506571f6
kosta2222 <muh_kosta@mail.ru>
2019-01-06 22:41:56 +0300

    added comments, fixed ReadMe info

--- main_server_decoder.py
+++ main_server_decoder.py
@@ -1,16 +1,39 @@
 #-*-coding: utf-8-*-
+"""
+Голосовая интерпритация
+Примечание:
+                        Для индификаторов переменных я попытался сделать наподобии Венгерской нотации т.е.
+                        идет инфо о типе , потом семантический смысл переменной , например
+                        str_char_op , int_ptr , mas_S_OpStack означает массив с типом String ,
+                        а mas_I_Or_S_resOpnZapis список ( массив ) с типами Int или String .
+                        Имена полей придваряются суффиксом fi ( field ) , например fi_dict_str_int_funcTable это
+                        поле - карта с сключом Str , а значением Int .
+                        Методы предваряются суффиксом me ( method ) . У функций и методов бывают сигнатуры в конце ,
+                        после буквы S ( signature ) , т.е. какие типы функция / метод принимает , например
+                        me_recurs_evalPerList_SMrV - метод recurs_evalPerList принимает M - массив / список ,
+                        возвращае Пустое т.е V ( void ) . После буквы r ( Return ) идет возвращаемое 
+                        функций / методом значение. I это всегда Int , D или F - вещественное число, S - строка
+"""
 from flask import Flask,render_template,request
-from S_compiler import *
-import libTestPydModuleFloat as vt
+from S_compiler import * # компилятор Ukuvchi
+import libTestPydModuleFloat as vt # VM Ukuvchi
 import os
+# обект веб-сервера(фреймоврка) Flask
 obj_serverApp=Flask(__name__,static_folder='static')
+# вызываемый метод
 @obj_serverApp.route('/vizovSait_decode',methods=["POST"])
 def vizovSait_decode():
+    """
+    Sym-expression == (S_funcName S S S ...) смотри Lisp
+    S_gotFromBrowser_ZnachfuSexpr==S_funcName ma ->  строка от браузера
+    S_gotFromBrowser_restSexp==S S S ... -> от строка  от браузера
+    """
     S_gotFromBrowser_ZnachfuSexpr= request.form['znachfuSexpr']
     S_gotFromBrowser_restSexp= request.form['restSexpr']
-    print('rest:',S_gotFromBrowser_restSexp)
+    # разбиваем S S S ... -> mas_S_
     mas_S_gotFromBrowser_restSexp=S_gotFromBrowser_restSexp.split()
     I_count=0
+    # заменяем некоторые слова
     for i in mas_S_gotFromBrowser_restSexp:
         if i=='сложить':
             mas_S_gotFromBrowser_restSexp[I_count]=' + '
@@ -34,11 +57,11 @@ def vizovSait_decode():
             mas_S_gotFromBrowser_restSexp[I_count] = ' '         
         I_count+=1    
     S_gotFromBrowser_restSexp=""
+    # востанавливаем строку -> S S S ...
     for i in  mas_S_gotFromBrowser_restSexp:
        S_gotFromBrowser_restSexp+=i+" " 
         
-    print('raw WebPars:',S_gotFromBrowser_ZnachfuSexpr,S_gotFromBrowser_restSexp)
-    
+    # запись в temp файл ддля сессий -> исходный текст ЯП
     fObj_CompilText=open('tmpFile_compilText.tmp','a')
     if  S_gotFromBrowser_ZnachfuSexpr=='$':
         fObj_CompilText.write('($ ')
@@ -49,9 +72,10 @@ def vizovSait_decode():
         fObj_CompilText.write("(callUserCos)")   
     if S_gotFromBrowser_ZnachfuSexpr=='z':
         fObj_CompilText.write("(print z)")          
-    if  S_gotFromBrowser_ZnachfuSexpr==')': 
-     fObj_CompilText.write(')')
-     fObj_CompilText.close() 
+    if  S_gotFromBrowser_ZnachfuSexpr==')': # программа закончена
+     fObj_CompilText.write(')') 
+     fObj_CompilText.close() # закрываем файл
+     # читаем и выполняем
      fObj_CompilText=open('tmpFile_compilText.tmp','r')
      S_textProgram=fObj_CompilText.read()
      obj_LispMach=LispMach()
@@ -59,23 +83,17 @@ def vizovSait_decode():
      vectorKintK_opCode=obj_LispMach.me_ret_byteCode_SVrL()
      vectorKintK_opCode.append(HALT)
      vt.eval(vectorKintK_opCode,obj_LispMach.fi_int_startIp,0)
-     fObj_CompilText.close()
+     fObj_CompilText.close() # закрываем файл
      with  open('tmpFile_compilText.tmp','w') as fObj_CompilText:
-         fObj_CompilText.truncate(0)
-     #if I_gotFromBrowser==2:
-      #print("Zapusk blocknota")
-      #os.system("start cmd /C \"D:/Programs/npp.7.4.1.bin/notepad++.exe\"")
-      
-    #elif I_gotFromBrowser==3:
-        #print("Zakritie blocknota")
-        #os.system("%SystemRoot%/system32/taskkill /f /im notepad++.exe")
+         fObj_CompilText.truncate(0) # затираем фаил
     return "Ok"
-   
+# просто страница   
 @obj_serverApp.route('/')
 def home():
     return ""
 
 
 if __name__=='__main__':
+    """ запускаем сервер"""
     obj_serverApp.run(host="0.0.0.0",debug=True,port=os.environ.get('PORT',5000))
     
